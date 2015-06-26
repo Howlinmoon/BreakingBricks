98c6629b83e9436fab6a83761320a75b2f859cab
Jim Veneskey <github@strifecrafter.com>
2015-06-25 20:49:11 -0400

    Created a simple sprite / node and placed it at the default co-ordinates

--- BreakingBricks/MyScene.m
+++ BreakingBricks/MyScene.m
@@ -13,6 +13,14 @@
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
+        self.backgroundColor = [SKColor whiteColor];
+        
+        // Creating a new sprite node from an image file
+        SKSpriteNode *ball = [SKSpriteNode spriteNodeWithImageNamed:@"ball"];
+        
+        // Add the new sprite node to the scene
+        [self addChild:ball];
+        
         
     }
     return self;
