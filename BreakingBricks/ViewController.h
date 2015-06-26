73fd7baab6e88989e748310dda64d259fef4ec50
Jim Veneskey <github@strifecrafter.com>
2015-06-25 20:41:31 -0400

    Initial real commit

---
+++ BreakingBricks/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  BreakingBricks
+//
+
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BreakingBricks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BreakingBricks
+//
+//  Created by Jim Veneskey on 6/25/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
